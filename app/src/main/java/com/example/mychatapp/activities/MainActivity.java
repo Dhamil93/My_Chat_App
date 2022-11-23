3188589da09bff249a852f74d1faef54cb229b22
dhamil93 <damilareezekile.atilola@student.griffith.ie>
2022-11-23 18:40:03 +0000

    Work in progress

--- app/src/main/java/com/example/mychatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/mychatapp/activities/MainActivity.java
@@ -2,15 +2,88 @@ package com.example.mychatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-import com.example.mychatapp.R;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.util.Base64;
+import android.widget.Toast;
+
+import com.example.mychatapp.databinding.ActivityMainBinding;
+import com.example.mychatapp.utilities.Database;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Preference preference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preference = new Preference(getApplicationContext());
+        loadUserDetails();
+        getToken();
+        setListeners();
     }
+
+    private void loadUserDetails(){
+        binding.textName.setText(preference.getString(Database.KEY_NAME));
+        byte[] bytes = Base64.decode(preference.getString(Database.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken(){
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void setListeners(){
+        binding.logout.setOnClickListener(view -> logOut());
+    }
+
+    public void updateToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Database.KEY_USERS).document(
+                        preference.getString(Database.USER_ID)
+                );
+        documentReference.update(Database.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token Updated"))
+                .addOnFailureListener(e -> showToast("Token can't be updated"));
+    }
+
+    private void logOut(){
+        showToast("Logging Out......😒😒😒");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                database.collection(Database.KEY_USERS).document(
+                        preference.getString(Database.USER_ID)
+                );
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Database.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preference.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> showToast("You can't log Out..... 😁😁😁"));
+    }
+
 }
--- app/src/main/java/com/example/mychatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/mychatapp/activities/SignInActivity.java
@@ -2,18 +2,32 @@ package com.example.mychatapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mychatapp.databinding.ActivitySignInBinding;
+import com.example.mychatapp.utilities.Database;
+import com.example.mychatapp.utilities.Preference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private Preference preference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preference = new Preference(getApplicationContext());
+        if(preference.getBoolean(Database.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -22,6 +36,63 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textOpenNewAccount.setOnClickListener(view ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.buttonLogIn.setOnClickListener(view ->{
+            if (isValidDetails()){
+                signIn();
+            }
+        } );
+    }
 
+    private void signIn(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Database.KEY_USERS)
+                .whereEqualTo(Database.KEY_EMAIL, binding.enterEmail.getText().toString())
+                .whereEqualTo(Database.KEY_PASSWORD, binding.enterPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task ->{
+                   if(task.isComplete() && task.getResult() != null && task.getResult().getDocuments()
+                           .size() > 0){
+                       DocumentSnapshot documentSnapshot =  task.getResult().getDocuments().get(0);
+                       preference.putBoolean(Database.KEY_IS_SIGNED_IN, true);
+                       preference.putString(Database.USER_ID, documentSnapshot.getId());
+                       preference.putString(Database.KEY_NAME, documentSnapshot.getString(Database.KEY_NAME));
+                       preference.putString(Database.KEY_IMAGE, documentSnapshot.getString(Database.KEY_IMAGE));
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                       startActivity(intent);
+                   }else {
+                       loading(false);
+                       showToast("You can't be signed In");
+                   }
+                });
+    }
+
+    private void loading(Boolean isLoading){
+        if(isLoading){
+            binding.buttonLogIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonLogIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+    private Boolean isValidDetails(){
+        if(binding.enterEmail.getText().toString().trim().isEmpty()){
+            showToast("Enter Email");
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.enterEmail.getText().toString()).matches()){
+            showToast("Enter a Valid Email");
+            return false;
+        }else if(binding.enterPassword.getText().toString().trim().isEmpty()){
+            showToast("Insert Password");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
